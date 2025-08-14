11d6b4650fb30a4720e52d01648ae6faec841f1a
Mystery Demon <161893464+MysteryDemon@users.noreply.github.com>
2025-08-14 01:59:42 -0700

    Update utils.py

--- TorrentDL/helper/utils.py
+++ TorrentDL/helper/utils.py
@@ -195,7 +195,7 @@ async def handle_download_and_send(message, download, user_id, LOGS, status_mess
         "status_message": status_message,
         "cancelled": False
     }
-
+    metadata_name = download_metadata_names.get(getattr(download, 'gid', None), None)
     while not download.is_complete:
         if active_downloads[download_id].get("cancelled"):
             LOGS.info(f"Download cancelled for ID: {download_id}")
