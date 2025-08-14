9176f6a68e62a6a992775f7a27e3fae44757ba56
Mystery Demon <161893464+MysteryDemon@users.noreply.github.com>
2025-08-13 19:10:54 -0700

    Update mediainfo.py

--- TorrentDL/helper/mediainfo.py
+++ TorrentDL/helper/mediainfo.py
@@ -146,4 +146,82 @@ async def srm(c, m, text, photo=None, video=None, markup=None, reply_id=None, de
    return my
  except:
    LOGGER.error('srm', exc_info=True)
+
+from string import ascii_letters
+from random import SystemRandom
+from asyncio import sleep
+from telegraph.aio import Telegraph
+from telegraph.exceptions import RetryAfterError
+from bot import bot_loop
+from config import Config
+from __init__ import LOGGER
+
+class TelegraphHelper:
+    def __init__(self):
+        self.telegraph = Telegraph(domain='graph.org')
+        self.short_name = ''.join(SystemRandom().choices(ascii_letters, k=8))
+        self.access_token = None
+        self.author_name = Config.AUTHOR_NAME
+        self.author_url = Config.AUTHOR_URL
+
+    async def create_account(self):
+        await self.telegraph.create_account(
+            short_name=self.short_name,
+            author_name=self.author_name,
+            author_url=self.author_url
+        )
+        self.access_token = self.telegraph.get_access_token()
+        LOGGER.info(f"Telegraph Account Generated : {self.short_name}")
+
+    async def create_page(self, title, content):
+        try:
+            return await self.telegraph.create_page(
+                title=title,
+                author_name=self.author_name,
+                author_url=self.author_url,
+                html_content=content
+            )
+        except RetryAfterError as st:
+            LOGGER.warning(f'Telegraph Flood control exceeded. I will sleep for {st.retry_after} seconds.')
+            await sleep(st.retry_after)
+            return await self.create_page(title, content)
+
+    async def edit_page(self, path, title, content):
+        try:
+            return await self.telegraph.edit_page(
+                path=path,
+                title=title,
+                author_name=self.author_name,
+                author_url=self.author_url,
+                html_content=content
+            )
+        except RetryAfterError as st:
+            LOGGER.warning(f'Telegraph Flood control exceeded. I will sleep for {st.retry_after} seconds.')
+            await sleep(st.retry_after)
+            return await self.edit_page(path, title, content)
+
+    async def edit_telegraph(self, path, telegraph_content):
+        nxt_page = 1
+        prev_page = 0
+        num_of_path = len(path)
+        for content in telegraph_content:
+            if nxt_page == 1:
+                content += f'<b><a href="https://telegra.ph/{path[nxt_page]}">Next</a></b>'
+                nxt_page += 1
+            else:
+                if prev_page <= num_of_path:
+                    content += f'<b><a href="https://telegra.ph/{path[prev_page]}">Prev</a></b>'
+                    prev_page += 1
+                if nxt_page < num_of_path:
+                    content += f'<b> | <a href="https://telegra.ph/{path[nxt_page]}">Next</a></b>'
+                    nxt_page += 1
+            await self.edit_page(
+                path=path[prev_page],
+                title=f"{config_dict['TITLE_NAME']} Torrent Search",
+                content=content
+            )
+        return
+
+
+telegraph = TelegraphHelper()
      
